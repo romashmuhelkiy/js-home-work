229bdc58ae0cd7626741eb57087a6eaa0d708880
romashmuhelkiy <romashmuhelkiy@ukr.net>
2021-10-19 12:37:35 +0300

    home work

---
+++ dz(js)1/index.js
@@ -0,0 +1,50 @@
+let myString = "My string";
+console.log(myString);
+console.log(typeof myString);
+let myNumber = 123;
+console.log(myNumber);
+console.log(typeof myNumber);
+let myBoolean = true;
+console.log(myBoolean);
+console.log(typeof myBoolean);
+let myNull = null;
+console.log(myNull)
+console.log(typeof myNull);
+let a;
+console.log(a);
+console.log(typeof a);
+
+
+
+let string = "123";
+console.log(typeof Number(string));
+console.log(Number(string));
+
+let number = 123;
+console.log(typeof String(number));
+console.log(String(number));
+
+let nuMber = 125;
+console.log(typeof Boolean(nuMber));
+console.log(Boolean(nuMber));
+
+let age = 18;
+let age2 = 25;
+
+let aGe = +alert(age+age2);
+console.log(age+age2)
+
+let money = +prompt("how much money");
+let moNey = +prompt("how much money");
+let Money = +alert(Number(money+moNey));
+console.log(money+moNey);
+
+let mOney = +prompt("how money")
+let moneY = +alert(mOney/2)
+
+
+let question = confirm("Are you the man?")
+console.log(question)
+
+
+
